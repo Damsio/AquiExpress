9658fac86a3c83b6455337ec50789fb8bc576970
André Damásio <andreydamasio@gmail.com>
2020-02-27 16:11:53 -0300

    primeiro commit

---
+++ Loja/src/main/java/br/edu/ifrn/andre/LojaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.andre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaApplication {
+
+	public static void main(String[] aquiexpress) {
+		SpringApplication.run(LojaApplication.class, aquiexpress);
+	}
+
+}
